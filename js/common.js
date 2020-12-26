943d1bb1edd628eec71a281e24fd20d3717dc69e
kudo1521 <agongteng73@gmail.com>
2020-12-26 18:09:01 +0900

    習熟度にアニメーションを実装

--- js/common.js
+++ js/common.js
@@ -1,5 +1,19 @@
-$('.field-title').click(function() {
-    $('#box').animate({
-    'marginTop': '200px'
+// $('.field-title').click(function() {
+//     $('#box').animate({
+//     'marginTop': '200px'
+//     });
+// });
+$(function(){
+    $(".score").css({opacity:"0"});
+    $(window).scroll(function (){
+        $('.score').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 100){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
     });
 });
