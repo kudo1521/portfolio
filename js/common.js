5f7e85d63259a2ffb248eb239bc5621a66a1af6e
kudo1521 <agongteng73@gmail.com>
2021-01-11 08:56:08 +0900

    タイピング風の文字の表示の実装が出来ない

--- js/common.js
+++ js/common.js
@@ -16,3 +16,17 @@ $(function(){
 $('.accordion').click(function () {
     $(this).next().slideToggle(300);
 });
+
+$(window).on('load',function(){
+    // ここから文字を<span></span>で囲む記述
+    $('.name').children().andSelf().contents().each(function() {
+    if (this.nodeType == 3) {
+    $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
+    }
+    });
+    // ここから一文字ずつフェードインさせる記述
+    $('.name').css({'opacity':1});
+    for (var i = 0; i <= $('.name').children().size(); i++) {
+    $('.name').children('span:eq('+i+')').delay(50*i).animate({'opacity':1},50);
+    };
+    });
