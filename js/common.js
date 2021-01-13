b65039f8bcdbde47f2840f0ddf8db933fa1c007f
kudo1521 <agongteng73@gmail.com>
2021-01-13 19:29:39 +0900

    最新版

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-$('.thanks p').fadeIn(5000);
+$('.thanks p').fadeIn(7000);
 $(function(){
     $(".score").css({opacity:"0"});
     $(window).scroll(function (){
@@ -17,16 +17,38 @@ $('.accordion').click(function () {
     $(this).next().slideToggle(300);
 });
 
-$(window).on('load',function(){
-    // ここから文字を<span></span>で囲む記述
-    $('.name').children().andSelf().contents().each(function() {
-    if (this.nodeType == 3) {
-    $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
-    }
+// アニメーションさせたいクラス
+var container = $(".name");
+// アニメーションスピード
+var speed = 300;
+// テキストの間にスペースを入れます
+var content = $(container).html();
+var text = $.trim(content);
+var newHtml = "";
+// スペースで区切ったテキストを、テキストの数だけspanで囲む
+text.split("").forEach(function(v) {
+newHtml += '<span>' + v + '</span>';
+});
+// spanで囲んだテキスト群をHTMLに戻す
+$(container).html(newHtml);
+// 1文字ずつ表示
+var txtNum = 0;
+setInterval(function() {
+$(container).find('span').eq(txtNum).css({opacity: 1});
+txtNum++
+}, speed);
+
+$(function(){
+    $(".ef").css({opacity:"0"});
+    $(window).scroll(function (){
+        $('.ef').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
     });
-    // ここから一文字ずつフェードインさせる記述
-    $('.name').css({'opacity':1});
-    for (var i = 0; i <= $('.name').children().size(); i++) {
-    $('.name').children('span:eq('+i+')').delay(50*i).animate({'opacity':1},50);
-    };
-    });
+});
