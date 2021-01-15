96aa1162671f33851eaf1d4ab86b612bddb7e3ad
kudo1521 <agongteng73@gmail.com>
2021-01-16 07:59:37 +0900

    目次の下線を追加 スムーススクロールをjsで実装

--- js/common.js
+++ js/common.js
@@ -52,3 +52,15 @@ $(function(){
         });
     });
 });
+// スムーススクロール
+$(function(){
+    // #で始まるリンクをクリックしたら実行されます
+    $('a[href^="#"]').click(function() {
+        var speed = 400; // ミリ秒で記述
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({scrollTop:position}, speed, 'swing');
+        return false;
+    });
+});
